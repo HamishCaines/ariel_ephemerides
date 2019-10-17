eacaf46ad874ddaf1db99e653a78537e29297f1e
Hamish Caines <hamish.caines.14@ucl.ac.uk>
2019-10-17 12:38:09 +0100

    Added handling for simulate mode

--- tools.py
+++ tools.py
@@ -46,37 +46,46 @@ class OverspecifiedInputsError(Exception):
     pass
 
 
+class InvalidArgumentError(Exception):
+    pass
+
+
 def check_input_dates(args):
     from datetime import datetime, timedelta
-    # check for over specification
-    if args.st is not None and args.ed is not None and args.wl is not None:
-        raise OverspecifiedInputsError
-    # work from today through window
-    if args.st is None and args.ed is None:
-        if args.wl is None:
-            raise UndefinedStartDateError
-        else:
-            start = datetime.today()
-            end = start + timedelta(days=args.wl)
-    # work from specified date through window
-    elif args.st is not None and args.ed is None:
-        if args.wl is None:
-            raise UndefinedEndDateError
-        else:
-            start = datetime.strptime(args.st, '%Y-%m-%d')
-            end = start + timedelta(days=args.wl)
+    if args.mode == 'simulate':
+        start = datetime(year=2019, month=6, day=12)
+        end = datetime(year=2030, month=6, day=12)
+
+    else:
+        # check for over specification
+        if args.st is not None and args.ed is not None and args.wl is not None:
+            raise OverspecifiedInputsError
+        # work from today through window
+        if args.st is None and args.ed is None:
+            if args.wl is None:
+                raise UndefinedStartDateError
+            else:
+                start = datetime.today()
+                end = start + timedelta(days=args.wl)
+        # work from specified date through window
+        elif args.st is not None and args.ed is None:
+            if args.wl is None:
+                raise UndefinedEndDateError
+            else:
+                start = datetime.strptime(args.st, '%Y-%m-%d')
+                end = start + timedelta(days=args.wl)
+                if end < datetime.today():
+                    raise StartingInPastError
+        # work from today to end date
+        elif args.st is None and args.ed is not None:
+            end = datetime.strptime(args.ed, '%Y-%m-%d')
             if end < datetime.today():
                 raise StartingInPastError
-    # work from today to end date
-    elif args.st is None and args.ed is not None:
-        end = datetime.strptime(args.ed, '%Y-%m-%d')
-        if end < datetime.today():
-            raise StartingInPastError
+            else:
+                start = datetime.today()
+        # work between two dates
         else:
-            start = datetime.today()
-    # work between two dates
-    else:
-        start = datetime.strptime(args.st, '%Y-%m-%d')
-        end = datetime.strptime(args.ed, '%Y-%m-%d')
+            start = datetime.strptime(args.st, '%Y-%m-%d')
+            end = datetime.strptime(args.ed, '%Y-%m-%d')
 
     return start, end
