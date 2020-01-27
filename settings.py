85b14b42926f8e12e656dc82afb9e5dccd142af7
Hamish Caines <hamish.caines.14@ucl.ac.uk>
2020-01-27 15:41:35 +0000

    Added handling for more advanced threshold settings

--- settings.py
+++ settings.py
@@ -4,7 +4,8 @@ class Settings:
 
         self.mode = None
         self.telescopes = None
-        self.threshold = None
+        self.threshold_mode = None
+        self.threshold_value = None
         self.start = None
         self.end = None
         self.window = None
@@ -27,9 +28,10 @@ class Settings:
                         self.mode = val
                     elif key == 'TELESCOPES':
                         self.telescopes = val
-
-                    elif key == 'THRESHOLD':
-                        self.threshold = int(val)
+                    elif key == 'THRESH_MODE':
+                        self.threshold_mode = val
+                    elif key == 'THRESH_VALUE':
+                        self.threshold_value = int(val)
                     elif key == 'START':
                         self.start = datetime.strptime(val, '%Y-%m-%d').replace(hour=0, minute=0, second=0, microsecond=0)
                     elif key == 'END':
@@ -49,8 +51,11 @@ class Settings:
         if self.mode != 'SCHEDULE' and self.mode != 'SIMULATE':
             print('Invalid mode specified, must be either SCHEDULE or SIMULATE')
             raise Exception
-        if self.threshold is None:
-            print('No Accuracy Threshold specified')
+        if self.threshold_mode != 'FIXED' and self.threshold_mode != 'SIGMA':
+            print('Invalid Threshold Mode, must be either FIXED or SIGMA')
+            raise Exception
+        if self.threshold_value is None:
+            print('No Accuracy Threshold value specified')
             raise Exception
         if self.telescopes is None:
             print('No Telescope file specified')
@@ -103,7 +108,7 @@ class Settings:
 
     def obtain_directory_name(self):
         from datetime import datetime
-        directory_name_base = f'{self.simulation_method}_N{self.telescopes.split(".")[0]}_T{str(self.threshold)}'
+        directory_name_base = f'{self.simulation_method}_N{self.telescopes.split(".")[0]}_T{str(self.threshold_value)}'
         run_datetime = datetime.today()
         run_datetime_str = f'{run_datetime.date()}T{run_datetime.time()}'
         directory_name = f'{directory_name_base}_{run_datetime_str.split(".")[0].replace(":", "-")}'
